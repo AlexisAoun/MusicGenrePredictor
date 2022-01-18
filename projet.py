3e77d7e071fca357bd13a82bdace576cbaeef080
alex292802 <81558393+alex292802@users.noreply.github.com>
2022-01-18 15:45:33 +0100

    feat: add main file

---
+++ projet.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 18 14:16:05 2022
+
+@author: aberg
+"""
+
+import numpy as np
+import pandas as pd
+
+ds_test=pd.read_csv("spotify_dataset_test.csv")
+ds_train=pd.read_csv("spotify_dataset_train.csv")
+ds_subset=pd.read_csv("spotify_dataset_subset.csv")
+
+print("Taille du dataset_test :",np.shape( ds_test))
+print("Taille du dataset_train :",np.shape(ds_train))
+#on a une colonne en plus correspondant au genre 
+print("Taille du dataset dataset_subset :",np.shape(ds_subset))
+
+
+ds_test.info()
+ds_train.info()
+ds_subset.info()
+
+
+print(ds_test.head())
