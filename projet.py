c82be370e21c4c1d3638ddb53fab56adbcd1340e
alex292802 <81558393+alex292802@users.noreply.github.com>
2022-01-20 10:45:44 +0100

    feat: ajout de plusieurs classifieurs et de f-fold

--- projet.py
+++ projet.py
@@ -3,11 +3,30 @@
 Created on Tue Jan 18 14:16:05 2022
 
 @author: aberg
+
+Random Forest
+Naive Bayes
+Stochastic Gradient Descent Classifier
+Logistic Regression
+
 """
 
 import numpy as np
 import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
+
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import KFold
+
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+
 
 ds_test=pd.read_csv("data/spotify_dataset_test.csv")
 ds_train=pd.read_csv("data/spotify_dataset_train.csv")
@@ -23,11 +42,46 @@ ds_test=ds_test.drop(columns = ['release_date','explicit','mode','key'])
 ds_train=ds_train.drop(columns = ['release_date','explicit','mode','key','genre'])
 
 
-#Essai avec le randomforest
-clf = RandomForestClassifier()
-clf.fit(ds_train, y)
+#1er essais avec un train_test_split et quelques classifieurs
+X_train, X_test, y_train, y_test = train_test_split(ds_train,y)
+#clf = RandomForestClassifier(n_estimators=100, criterion='gini', max_depth=None,random_state=0)
+clf = KNeighborsClassifier(n_neighbors=1000)
+#clf=LinearDiscriminantAnalysis()
+#clf = QuadraticDiscriminantAnalysis()
+#clf = LogisticRegression(random_state=0)
+#clf= GaussianNB()
+clf.fit(X_train, y_train)
+y_predict=clf.predict(X_test)
 
-print(clf.predict(ds_test))
+
+
+
+
+
+
+
+print("-Accuracy : ", accuracy_score(y_test, y_predict))
+
+
+'''
+#kfolds
+scores = []
+clf = RandomForestClassifier(n_estimators=100, criterion='entropy')
+cv = KFold(n_splits=10)
+for train_index, test_index in cv.split(ds_train) :
+    print(train_index)
+    print(ds_train)
+    X_train=ds_train.iloc[train_index]
+    y_train=y.iloc[train_index]
+    X_test=ds_train.iloc[test_index]
+    y_test=y.iloc[test_index]
+    clf.fit(X_train, y_train)
+    scores.append(clf.score(X_test, y_test))
+
+print("results",scores)
+
+'''
+    
 
 
 
