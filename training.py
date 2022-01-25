37bc7bdff6736174177fcd1bff900b39ce71f9ec
Alexis Aoun <alexis.aoun@hotmail.com>
2022-01-25 17:35:28 +0100

    fix(train): correct model name

--- training.py
+++ training.py
@@ -5,9 +5,9 @@ m = model.getModel()
 trainX, trainY, testX, testY = datasetPrep.computeDatasets()
 
 def train(epochs=50):
-    model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-    model.fit(trainX, trainY, epochs=epochs)
-    model.save('models/modelTest')
+    m.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+    m.fit(trainX, trainY, epochs=epochs)
+    m.save('models/modelTest')
 
 train()
 score = m.evaluate(testX, testY, verbose=0)
