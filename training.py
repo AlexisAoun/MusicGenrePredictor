8c4b5e916b9fcf3d8af19c1b1a57912ec0106303
Alexis Aoun <alexis.aoun@hotmail.com>
2022-01-25 17:33:08 +0100

    feat(train): add evaluate at the end of training

--- training.py
+++ training.py
@@ -2,11 +2,14 @@ import model
 import datasetPrep
 
 m = model.getModel() 
+trainX, trainY, testX, testY = datasetPrep.computeDatasets()
 
 def train(epochs=50):
     model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-    trainX, trainY, testX, testY = datasetPrep.computeDatasets()
     model.fit(trainX, trainY, epochs=epochs)
     model.save('models/modelTest')
 
 train()
+score = m.evaluate(testX, testY, verbose=0)
+print("Test loss:", score[0])
+print("Test accuracy:", score[1])
