112c96c873c6af5c0b91eb0e8dc81e5e1827726a
quent1fvr <quentinfever@icloud.com>
2022-01-25 15:21:39 +0100

    SVc change

--- Svc.py
+++ Svc.py
@@ -4,6 +4,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import GridSearchCV
 from sklearn import model_selection
 from sklearn.svm import SVC
+from sklearn.metrics import classification_report, confusion_matrix
 
 
 
@@ -13,13 +14,14 @@ ds_train = pd.read_csv("data/spotify_dataset_train.csv")
 y_train = ds_train.genre
 x_train = ds_train[ds_train.columns[:-1]]
 x_train = x_train.drop("release_date",axis=1)
-x_train = x_train.drop("explicit",axis=1)
-x_train = x_train.drop("duration_ms",axis=1)
+#x_train = x_train.drop("explicit",axis=1)
+#x_train = x_train.drop("duration_ms",axis=1)
 
 XTrain,XTest,yTrain,yTest = model_selection.train_test_split(x_train,y_train,test_size=500,random_state=1)
 
 param_grid = {'C':[1,10], 'gamma':[0.1,0.01], "kernel": ["rbf"]}
-  
+
+"""  
  
 grid = GridSearchCV(SVC(), param_grid, refit = True, verbose = 3)
  
@@ -32,3 +34,13 @@ print(grid.best_params_)
 # print how our model looks after hyper-parameter tuning
 print(grid.best_estimator_)
 print(grid.best_score_) 
+
+"""
+
+svclassifier = SVC(kernel='poly', degree=3)
+svclassifier.fit(XTrain, yTrain)
+y_pred = svclassifier.predict(XTest)
+
+print(confusion_matrix(yTest,y_pred))
+print(classification_report(yTest,y_pred))
+print(accuracy_score(yTest,y_pred))
